87a9c7141fcbff4c9431d6e2c16177e4bb15a362
deepakr <deepak.r@sakshath-technologies.com>
2025-03-05 15:27:21 +0530

    Unit testing

--- cabs/cabs/doctype/driver/driver.py
+++ cabs/cabs/doctype/driver/driver.py
@@ -1,9 +1,15 @@
 # Copyright (c) 2025, Deepak R and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Driver(Document):
-	pass
+	def before_save(self):
+		if self.last_name:
+			self.full_name=f'{self.first_name} {self.last_name}'
+		else:
+			self.full_name=f'{self.first_name}'
+		
+		
--- cabs/cabs/doctype/driver/test_driver.py
+++ cabs/cabs/doctype/driver/test_driver.py
@@ -1,9 +1,28 @@
 # Copyright (c) 2025, Deepak R and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
 
 
 class TestDriver(FrappeTestCase):
-	pass
+
+	def test_full_name(self):
+		test_driver = frappe.new_doc("Driver")
+		test_driver.first_name="Bin"
+		test_driver.last_name="Sharma"
+		test_driver.save()
+		self.assertEqual(test_driver.full_name,"Bin Sharma")
+
+	def test_name_without_last_name(self):
+		test_driver = frappe.new_doc("Driver")
+		test_driver.first_name="Ravi"
+		# test_driver.last_name="Sharma"
+		test_driver.save()
+		self.assertEqual(test_driver.full_name,"Ravii")
+
+			
+
+
+		
+	
